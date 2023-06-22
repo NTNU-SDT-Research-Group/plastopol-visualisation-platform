d5ac446f2f9c43ee00368db3c1cad13324affc3c
Pipe-Runner <piperunner.in@gmail.com>
2023-06-22 21:07:15 +0200

    Added file upload mechanims

--- src/app/api/annotation/route.ts
+++ src/app/api/annotation/route.ts
@@ -1,9 +1,40 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import fs from "fs";
 
-export async function GET() {
-  const res = "Hello";
+// https://github.com/vercel/next.js/discussions/39957
+export async function POST(request: NextRequest) {
+  try {
+    // * INFO: Form data is ordered when pushed in single key
+    const formData = await request.formData();
 
-  return NextResponse.json(res);
+    formData.getAll("images").forEach(async (blob) => {
+      const file = blob as File;
+      const fileName = `${Date.now()}-${file.name}`;
+      const path = `upload/${fileName}`;
+      const arrayBuffer = await file.arrayBuffer();
+
+      fs.writeFileSync(`public/${path}`, Buffer.from(arrayBuffer));
+
+      const createdEntry = await prisma.imageWithAnnotations.create({
+        data: {
+          imageUrl: path,
+        },
+      });
+
+      console.log(createdEntry);
+    });
+
+    // console.log(JSON.parse(formData.get("annotations") as string));
+
+    return NextResponse.json({
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json(
+      { error: `Upload failed - ${err}` },
+      { status: 500 }
+    );
+  }
 }
